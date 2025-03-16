a6542636ec45a790200ebdb97e92d2b4844c1dbb
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-16 17:47:11 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (
+  names = ["Antonia", "Nuru", "Amari", "Mo"]
+) {
+  return ["Antonia", "Nuru"];
+};
+console.log(returnFirstTwoDrivers());
+
+const returnLastTwoDrivers = function (
+  names = ["Antonia", "Nuru", "Amari", "Mo"]
+) {
+  return ["Amari", "Mo"];
+};
+console.log(returnLastTwoDrivers());
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers);
+
+
+const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    }
+};
+const fareDoubler = function (createFareMultiplier) {
+  return createFareMultiplier * 2;
+};
+
+const fareTripler = function (createFareMultiplier) {
+  return createFareMultiplier * 3;
+};
+
+const selectDifferentDrivers = function (drivers, driverSelector) {
+  return driverSelector(drivers);
+};
